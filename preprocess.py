637232b4f871763e8ba04fb062d4ea424915990a
willallstet <willallstetter@gmail.com>
2021-12-04 14:24:39 -0500

    add files and return as 3D list

--- preprocess.py
+++ preprocess.py
@@ -5,6 +5,7 @@ def preprocess(filepath):
     years_dict = {}
     data = pandas.read_csv(filepath)
     data = data.sort_values(by="GAME_DATE_EST") #sort by reverse order date
+    teamIDs = set()
     i = 0
     for row in data.itertuples():
         if row.SEASON in years_dict:
@@ -19,15 +20,22 @@ def preprocess(filepath):
             if not math.isnan(home_data[0]):
                 years_dict[row.SEASON][row.GAME_ID] = [row.HOME_TEAM_WINS, home_data, away_data]
                 i+=1
-                years_dict[row.SEASON] = {row.HOME_TEAM_ID: home_data} #averge stats of season so far
-                years_dict[row.SEASON] = {row.VISITOR_TEAM_ID: away_data} #averge stats of season so far
+                years_dict[row.SEASON][row.HOME_TEAM_ID] =  home_data #averge stats of season so far
+                years_dict[row.SEASON][row.VISITOR_TEAM_ID] = away_data #averge stats of season so far
         else: #start the season with the first game data
             home_data = [row.PTS_home,row.FG_PCT_home,row.FT_PCT_home,row.FG3_PCT_home,row.AST_home, row.REB_home]
             away_data = [row.PTS_away,row.FG_PCT_away,row.FT_PCT_away,row.FG3_PCT_away,row.AST_away, row.REB_away]
             years_dict[row.SEASON] = {row.GAME_ID: [row.HOME_TEAM_WINS, home_data, away_data]} #save game result in dict with team stats
-            years_dict[row.SEASON] = {row.HOME_TEAM_ID: home_data} #save the team data to average later
-            years_dict[row.SEASON] = {row.VISITOR_TEAM_ID: away_data} #save the team data to average later
+            years_dict[row.SEASON][row.HOME_TEAM_ID] = home_data #save the team data to average later
+            years_dict[row.SEASON][row.VISITOR_TEAM_ID] = away_data #save the team data to average later
             i=1
-    return years_dict
-
-preprocess("/Users/williamallstetter/Desktop/school/CS1470/final project/archive/games.csv")
+    returnArr = []
+    for season in years_dict:
+        season_data = []
+        for game in years_dict[season]:
+            if game not in teamIDs:
+                season_data.append(years_dict[season][game])
+        returnArr.append(season_data)
+    return season_data
+    
+preprocess("archive/games.csv")
