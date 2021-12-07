4c69101776902d56b2012ab2df5f9a69aa69c9d7
willallstet <willallstetter@gmail.com>
2021-12-07 16:44:06 -0500

    fix preprocess

--- preprocess.py
+++ preprocess.py
@@ -6,7 +6,6 @@ def preprocess(filepath):
     data = pandas.read_csv(filepath)
     data = data.sort_values(by="GAME_DATE_EST") #sort by reverse order date
     teamIDs = set()
-    i = 0
     for row in data.itertuples():
         if row.SEASON in years_dict:
             if row.HOME_TEAM_ID in years_dict[row.SEASON]:
@@ -18,14 +17,15 @@ def preprocess(filepath):
             else:
                 away_data = [row.PTS_away,row.FG_PCT_away,row.FT_PCT_away,row.FG3_PCT_away,row.AST_away, row.REB_away]
             if not math.isnan(home_data[0]):
-                years_dict[row.SEASON][row.GAME_ID] = [row.HOME_TEAM_WINS, home_data, away_data]
-                i+=1
+                years_dict[row.SEASON][row.GAME_ID] = [row.HOME_TEAM_WINS, row.HOME_TEAM_ID, row.VISITOR_TEAM_ID, home_data, away_data]
+                #print(years_dict[row.SEASON][row.GAME_ID])
                 years_dict[row.SEASON][row.HOME_TEAM_ID] =  home_data #averge stats of season so far
                 years_dict[row.SEASON][row.VISITOR_TEAM_ID] = away_data #averge stats of season so far
         else: #start the season with the first game data
             home_data = [row.PTS_home,row.FG_PCT_home,row.FT_PCT_home,row.FG3_PCT_home,row.AST_home, row.REB_home]
             away_data = [row.PTS_away,row.FG_PCT_away,row.FT_PCT_away,row.FG3_PCT_away,row.AST_away, row.REB_away]
-            years_dict[row.SEASON] = {row.GAME_ID: [row.HOME_TEAM_WINS, home_data, away_data]} #save game result in dict with team stats
+            years_dict[row.SEASON] = {row.GAME_ID: [row.HOME_TEAM_WINS, row.HOME_TEAM_ID, row.VISITOR_TEAM_ID, home_data, away_data]} #save game result in dict with team stats
+            #print(years_dict[row.SEASON][row.GAME_ID])
             years_dict[row.SEASON][row.HOME_TEAM_ID] = home_data #save the team data to average later
             years_dict[row.SEASON][row.VISITOR_TEAM_ID] = away_data #save the team data to average later
             i=1
