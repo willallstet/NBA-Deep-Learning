64c03c0abcd807147c66646083c96d674968aaef
willallstet <willallstetter@gmail.com>
2021-12-08 12:56:54 -0500

    added date

--- preprocess.py
+++ preprocess.py
@@ -1,3 +1,4 @@
+import datetime
 import pandas
 import math
 import numpy as np
@@ -9,6 +10,7 @@ def preprocess_games(filepath):
     data = data.sort_values(by="Date") #sort by reverse order date
     teamIDs = set()
     for row in data.itertuples():
+        date = row.Date.strftime("%m%d")
         if row.SEASON in years_dict:
             if row.HOME_TEAM_ID in years_dict[row.SEASON]:
                 i = years_dict[row.SEASON][row.HOME_TEAM_ID][0]
@@ -21,13 +23,13 @@ def preprocess_games(filepath):
             else:
                 away_data = [0, row.PTS_away,row.FG_PCT_away,row.FT_PCT_away,row.FG3_PCT_away,row.AST_away, row.REB_away]
             if not math.isnan(home_data[0]):
-                years_dict[row.SEASON][row.GAME_ID] = [row.HOME_TEAM_WINS, row.HOME_TEAM_ID, row.VISITOR_TEAM_ID, home_data[1:], away_data[1:]]
+                years_dict[row.SEASON][row.GAME_ID] = [date,row.HOME_TEAM_WINS, row.HOME_TEAM_ID, row.VISITOR_TEAM_ID, home_data[1:], away_data[1:]]
                 years_dict[row.SEASON][row.HOME_TEAM_ID] =  home_data #average stats of season so far
                 years_dict[row.SEASON][row.VISITOR_TEAM_ID] = away_data #average stats of season so far
         else: #start the season with the first game data
             home_data = [0,row.PTS_home,row.FG_PCT_home,row.FT_PCT_home,row.FG3_PCT_home,row.AST_home, row.REB_home]
             away_data = [0,row.PTS_away,row.FG_PCT_away,row.FT_PCT_away,row.FG3_PCT_away,row.AST_away, row.REB_away]
-            years_dict[row.SEASON] = {row.GAME_ID: [row.HOME_TEAM_WINS, row.HOME_TEAM_ID, row.VISITOR_TEAM_ID, home_data[1:], away_data[1:]]} #save game result in dict with team stats
+            years_dict[row.SEASON] = {row.GAME_ID: [date,row.HOME_TEAM_WINS, row.HOME_TEAM_ID, row.VISITOR_TEAM_ID, home_data[1:], away_data[1:]]} #save game result in dict with team stats
             years_dict[row.SEASON][row.HOME_TEAM_ID] = home_data #save the team data to average later
             years_dict[row.SEASON][row.VISITOR_TEAM_ID] = away_data #save the team data to average later
             i=1
